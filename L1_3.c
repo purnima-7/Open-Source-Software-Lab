b72164eb60a655ff60e98d5513c3d36712a3ddf5
Purnima Singh <purnimasingh1519@gmail.com>
2025-07-28 10:33:37 +0530

    Add files via upload

---
+++ L1_3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char str[100], temp;
+    int i, j;
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    str[strcspn(str, "\n")] = '\0';
+    int len = strlen(str);
+    for(i = 0, j = len - 1; i < j; i++, j--) {
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+    }
+    printf("Reversed string: %s\n", str);
+    return 0;
+}
